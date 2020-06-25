2e3f0ccead92f70abc1c09292304388e3d858116
lujf <lujf@akulaku.com>
2020-06-25 23:36:04 +0800

    首次提交

---
+++ elasticsearch-module/src/main/java/org/patric/daydayup/DaydayupApplication.java
@@ -0,0 +1,13 @@
+package org.patric.daydayup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaydayupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaydayupApplication.class, args);
+    }
+
+}
---
+++ elasticsearch-module/src/test/java/org/patric/daydayup/DaydayupApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patric.daydayup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaydayupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
